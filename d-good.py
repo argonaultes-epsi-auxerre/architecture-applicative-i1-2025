80f0bb297005c09719ed59bebc46f4edff1a5974
argonaultes <gael@argonaultes.fr>
2025-02-27 11:55:53 +0100

    dependency fixed

---
+++ d-good.py
@@ -0,0 +1,28 @@
+from abc import ABC, abstractmethod
+
+class Converter(ABC):
+    @abstractmethod
+    def convert(self, from_currency, to_currency, amount):
+        pass
+
+class FXConverter(Converter):
+    def convert(self, from_currency, to_currency, amount):
+        print(f'{amount} {from_currency} = {amount * 1.2} {to_currency}')
+        return amount * 1.2
+
+class DXConverter(Converter):
+    def convert(self, from_currency, to_currency, amount):
+        print(f'{amount} {from_currency} = {amount * 1.3} {to_currency}')
+        return amount * 1.3
+
+class App:
+    def __init__(self, converter : Converter):
+        self.__converter = converter
+
+    def start(self):
+        self.__converter.convert('EUR', 'USD', 100)
+
+
+if __name__ == '__main__':
+    app = App(converter = DXConverter())
+    app.start()
