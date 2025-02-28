cc1a3af5193f7319db707f62fd8463b3c85f29b9
argonaultes <gael@argonaultes.fr>
2025-02-28 18:01:22 +0100

    menu composite

--- design-pattern/composite/menu.py
+++ design-pattern/composite/menu.py
@@ -1,12 +1,10 @@
 from abc import ABC, abstractmethod
 
-class MenuItem:
+class MenuComponent(ABC):
 
-    def __init__(self, name, description, vegetarian, price):
+    def __init__(self, name, description):
         self.__name = name
         self.__description = description
-        self.__vegetarian = vegetarian
-        self.__price = price
 
     @property
     def name(self):
@@ -15,6 +13,17 @@ class MenuItem:
     @property
     def description(self):
         return self.__description
+
+    @abstractmethod
+    def print_menu(self):
+        pass
+
+class MenuItem(MenuComponent):
+
+    def __init__(self, name, description, vegetarian, price):
+        super().__init__(name, description)
+        self.__vegetarian = vegetarian
+        self.__price = price
 
     @property
     def vegetarian(self):
@@ -25,12 +34,10 @@ class MenuItem:
         return self.__price
 
     def print_menu(self):
-        return f'{self.__name} ({self.__description}) - {self.__price}'
+        print(f'{self.name} ({self.description}) - {self.__price}')
 
     def __repr__(self):
-        return f'{self.__name} ({self.__description}) - {self.__price}'
-
-
+        return f'{self.name} ({self.description}) - {self.__price}'
 
 class Iterator(ABC):
 
@@ -42,87 +49,83 @@ class Iterator(ABC):
     def has_more_items(self) -> bool:
         pass
 
-class IterableCollection(ABC):
 
-    @abstractmethod
+class Menu(MenuComponent):
+
+    def __init__(self, name, description):
+        super().__init__(name, description)
+        self.__menu_components = []
+
+    def add_menu_component(self, menu_component):
+        self.__menu_components.append(menu_component)
+
+    def print_menu(self):
+        print(f'== Menu: {self.name} ==')
+        it_menu = self.create_iterator()
+        while it_menu.has_more_items():
+            menu_item = it_menu.next_item()
+            menu_item.print_menu()
+
     def create_iterator(self) -> Iterator:
-        pass
+        return MenuIterator(self.__menu_components)
 
-class BreakfastMenuIterator(Iterator):
-    
-    def __init__(self, breakfast_menu_items):
-        self.__breakfast_menu_items = breakfast_menu_items
+class MenuIterator(Iterator):
+    def __init__(self, menu_items):
+        self.__menu_items = menu_items
         self.__position = 0
     
-    def next_item(self):
-        next_item = self.__breakfast_menu_items[self.__position]
+    def next_item(self) -> Menu:
+        next_item = self.__menu_items[self.__position]
         self.__position += 1
         return next_item
 
     def has_more_items(self):
-        return self.__position < len(self.__breakfast_menu_items)
+        return self.__position < len(self.__menu_items)
 
-class BreakfastMenu(IterableCollection):
 
-    def __init__(self):
-        self.__menu_items = []
-        self.add_item('pancake', 'the best', True, 10.0)
-        self.add_item('waffle', 'the best', True, 9.0)
-        self.add_item('kiwi', 'healthy and yellow', True, 2.0)
+class BreakfastMenu(Menu):
 
-    def add_item(self, name, description, vegetarian, price):
-        self.__menu_items.append(MenuItem(name, description, vegetarian, price))
+    def __init__(self, name, description):
+        super().__init__(name, description)
+        self.add_menu_component(MenuItem('pancake', 'the best', True, 10.0))
+        self.add_menu_component(MenuItem('waffle', 'the best', True, 9.0))
+        self.add_menu_component(MenuItem('kiwi', 'healthy and yellow', True, 2.0))
 
-    def create_iterator(self) -> Iterator:
-        return BreakfastMenuIterator(self.__menu_items)
 
-class LunchMenuIterator(Iterator):
-    def __init__(self, lunch_menu_items):
-        self.__lunch_menu_items = lunch_menu_items
-        self.__position = len(self.__lunch_menu_items) - 1
-    
-    def next_item(self):
-        next_item = self.__lunch_menu_items[self.__position]
-        self.__position -= 1
-        return next_item
+class LunchMenu(Menu):
 
-    def has_more_items(self):
-        return self.__position >= 0
+    def __init__(self, name, description):
+        super().__init__(name, description)
+        self.add_menu_component(MenuItem(name = 'burger', vegetarian=False, description='the best', price=10.0))
+        self.add_menu_component(MenuItem('chicken', 'cotcot', False, 12.0))
+        self.add_menu_component(MenuItem('tomato', 'sun and red', True, 2.0))
 
-class LunchMenu(IterableCollection):
 
-    def __init__(self):
-        self.__menu_items = []
-        self.add_item(name = 'burger', vegetarian=False, description='the best', price=10.0)
-        self.add_item('chicken', 'cotcot', False, 12.0)
-        self.add_item('tomato', 'sun and red', True, 2.0)
+class DinerMenu(Menu):
 
-    def add_item(self, name, description, vegetarian, price):
-        self.__menu_items.append(MenuItem(name, description, vegetarian, price))
-
-    def create_iterator(self):
-        return LunchMenuIterator(self.__menu_items)
-
+    def __init__(self, name, description):
+        super().__init__(name, description)
+        self.add_menu_component(MenuItem(name = 'salad', vegetarian=True, description='the best', price=10.0))
+        self.add_menu_component(MenuItem('soup', 'miam miam', True, 8.0))
+        self.add_menu_component(MenuItem('burgundy', 'very good', False, 22.0))
 
 class Waiter:
     
-    def __init__(self, breakfast_menu: BreakfastMenu, lunch_menu: LunchMenu):
-        self.__breakfast_menu = breakfast_menu
-        self.__lunch_menu = lunch_menu
+    def __init__(self, breakfast_menu: Menu, lunch_menu: Menu, diner_menu: Menu):
+        self.__menus = Menu('Tous les menus', '')
+        self.__menus.add_menu_component(breakfast_menu)
+        self.__menus.add_menu_component(lunch_menu)
+        self.__menus.add_menu_component(diner_menu)
 
     def display_menu(self):
-        it_breakfast_menu = self.__breakfast_menu.create_iterator()
-        print("==== Breakfast Menu ====")
-        while it_breakfast_menu.has_more_items():
-            menu_item = it_breakfast_menu.next_item()
-            print(menu_item)
+        self.__menus.print_menu()
 
-        it_lunch_menu = self.__lunch_menu.create_iterator()
-        print("==== Lunch Menu ====")
-        while it_lunch_menu.has_more_items():
-            menu_item = it_lunch_menu.next_item()
-            print(menu_item)
+    # def print_menu(self, menu : Menu):
+    #     it_menu = menu.create_iterator()
+    #     while it_menu.has_more_items():
+    #         menu_item = it_menu.next_item()
+    #         print(menu_item)
 
 if __name__ == '__main__':
-    jerome = Waiter(BreakfastMenu(), LunchMenu())
+    jerome = Waiter(BreakfastMenu('Breakfast', 'the most important'), LunchMenu('Lunch Menu', 'middle of the day'), DinerMenu('Diner Menu', 'go to bed'))
     jerome.display_menu()
