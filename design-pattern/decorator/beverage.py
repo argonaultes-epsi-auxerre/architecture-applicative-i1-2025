9dae7f59923d823fcc9c781f7e27eaa0a8558a55
argonaultes <gael@argonaultes.fr>
2025-02-28 11:17:26 +0100

    decorator done

--- design-pattern/decorator/beverage.py
+++ design-pattern/decorator/beverage.py
@@ -2,86 +2,98 @@ from abc import ABC, abstractmethod
 
 class Beverage(ABC):
 
-    def __init__(self, description, initial_cost):
+    def __init__(self, description):
         self.__description = description
-        self.__milk = False
-        self.__soy = False
-        self.__mocha = False
-        self.__sugar = False
-        self.__initial_cost = initial_cost
+
+    @property
+    def description(self):
+        return self.__description
+
+    @abstractmethod
+    def cost(self):
+        pass
+
+        # total_cost = self.__initial_cost
+
+        # if self.milk:
+        #     total_cost += 0.2
+
+        # if self.soy:
+        #     total_cost += 0.1
+
+        # if self.mocha:
+        #     total_cost += 10
+
+        # if self.sugar:
+        #     total_cost += 0.15
+
+        # return total_cost
+
+class Condiment(Beverage):
+
+    def __init__(self, beverage: Beverage, description):
+        super().__init__(description)
+        self._beverage = beverage
+
+class Soy(Condiment):
+    def __init__(self, beverage: Beverage):
+        super().__init__(beverage, 'soy')
 
     def cost(self):
-        total_cost = self.__initial_cost
+        return 0.1 + self._beverage.cost()
 
-        if self.milk:
-            total_cost += 0.2
+class Mocha(Condiment):
+    def __init__(self, beverage: Beverage):
+        super().__init__(beverage, 'mocha')
+    
+    def cost(self):
+        return 10 + self._beverage.cost()
 
-        if self.soy:
-            total_cost += 0.1
+class Milk(Condiment):
+    def __init__(self, beverage: Beverage):
+        super().__init__(beverage, 'milk')
+    
+    def cost(self):
+        return 0.2 + self._beverage.cost()
 
-        if self.mocha:
-            total_cost += 10
+class Sugar(Condiment):
+    def __init__(self, beverage: Beverage):
+        super().__init__(beverage, 'sugar')
 
-        if self.sugar:
-            total_cost += 0.15
-
-        return total_cost
-
-    @property
-    def sugar(self):
-        return self.__sugar
-
-    @sugar.setter
-    def sugar(self, value):
-        self.__sugar = value
-
-    @property
-    def milk(self):
-        return self.__milk
-
-    @milk.setter
-    def milk(self, value):
-        self.__milk = value
-
-    @property
-    def soy(self):
-        return self.__soy
-
-    @soy.setter
-    def soy(self, value):
-        self.__soy = value
-
-    @property
-    def mocha(self):
-        return self.__mocha
-
-    @mocha.setter
-    def mocha(self, value):
-        self.__mocha = value
+    def cost(self):
+        return 0.15 + self._beverage.cost()
 
 
 class Deca(Beverage):
 
     def __init__(self):
-        super().__init__('deca', 1.0)
+        super().__init__('deca')
 
-
+    def cost(self):
+        return 1.0
 
 class Americano(Beverage):
 
     def __init__(self):
-        super().__init__('americano', 2.0)
+        super().__init__('americano')
+
+    def cost(self):
+        return 2.0
 
 class Espresso(Beverage):
 
     def __init__(self):
-        super().__init__('espresso', 1.5)
-        self.soy = True
+        super().__init__('espresso')
+
+    def cost(self):
+        return 1.5
 
 
 class Chocolate(Beverage):
 
     def __init__(self):
-        super().__init__('chocolate', 3.0)
-        self.mocha = True
+        super().__init__('chocolate')
+
+    def cost(self):
+        return 3.0
 
--- design-pattern/decorator/columbuzz.py
+++ design-pattern/decorator/columbuzz.py
@@ -1,11 +1,12 @@
-from beverage import Deca, Americano, Espresso, Chocolate
+from beverage import Deca, Americano, Espresso, Chocolate, Milk, Soy, Mocha, Sugar
 
 
 if __name__ == '__main__':
     # serve 2 americano, 1 espresso
     # calculate the total cost
     americano_1 = Americano()
-    americano_2 = Americano()
-    americano_2.sugar = True
-    espresso_1 = Espresso()
-    print(f'total cost: {americano_1.cost() + americano_2.cost() + espresso_1.cost()} €')
+    americano_2 = Milk(Americano())
+#    americano_2.sugar = True
+    espresso_1 = Sugar(Soy(Espresso()))
+    print(f'total cost: {americano_1.cost() + americano_2.cost() + espresso_1.cost(): .2f} €')
+    print(f'{americano_1.cost()}')
