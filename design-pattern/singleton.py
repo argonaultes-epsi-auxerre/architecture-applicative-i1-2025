3274c8ab7dd122347273b9f8e3b1d53ef2232dc8
argonaultes <gael@argonaultes.fr>
2025-02-28 08:53:57 +0100

    singleton add

--- design-pattern/singleton.py
+++ design-pattern/singleton.py
@@ -4,7 +4,7 @@ class Chevre:
 # variable de classe
 # methode de classe
 
-class Berger:
+class BergerNaive:
     __berger = None
 
     @classmethod
@@ -12,6 +12,39 @@ class Berger:
         if cls.__berger is None:
             cls.__berger = Berger()
         return cls.__berger
+
+
+class SingletonBerger(type):
+    """
+    The Singleton class can be implemented in different ways in Python. Some
+    possible methods include: base class, decorator, metaclass. We will use the
+    metaclass because it is best suited for this purpose.
+    """
+
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+
+        print(f'_instances: {cls._instances}')
+        """
+        Possible changes to the value of the `__init__` argument do not affect
+        the returned instance.
+        """
+        if cls not in cls._instances:
+            instance = super().__call__(*args, **kwargs)
+            cls._instances[cls] = instance
+        return cls._instances[cls]
+
+
+class Berger(metaclass=SingletonBerger):
+    def some_business_logic(self):
+        """
+        Finally, any singleton should define some business logic, which can be
+        executed on its instance.
+        """
+
+        # ...
+
 
 
 class Troupeau:
@@ -28,6 +61,8 @@ if __name__ == '__main__':
     for _ in range(10):
         troupeau.add_chevre(Chevre())
     berger_1 = Berger()
+    print(f'berger_1: {berger_1}')
 
     berger_2 = Berger() # est-ce que berger_2 est bien identique à berger_1 ?
+    print(f'berger_2: {berger_2}')
     assert berger_1 == berger_2
